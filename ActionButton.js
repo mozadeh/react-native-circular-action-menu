0446e62bf4ee8dc46085a1365fae424f5e1c2efb
Mo Ghasemzadeh <mo.zadeh@berkeley.edu>
2020-06-29 16:08:47 -0400

    Update ActionButton.js

--- ActionButton.js
+++ ActionButton.js
@@ -81,7 +81,7 @@ export default class ActionButton extends Component {
     Animated.spring(this.state.anim, {
       toValue: 1,
       duration: 250,
-      useNativeDriver: true
+      useNativeDriver: false
     }).start();
 
     this.setState({ active: true });
@@ -91,7 +91,7 @@ export default class ActionButton extends Component {
     Animated.spring(this.state.anim, {
       toValue: 0,
       duration: 250,
-      useNativeDriver: true
+      useNativeDriver: false
     }).start();
 
     setTimeout(() => {
